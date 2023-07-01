49f7c15f1d7e36d6c4aaa2cfec02abdc286f303d
jmarkfen <119049235+jmarkfen@users.noreply.github.com>
2023-07-01 11:45:37 +0800

    Revert "Closes #2"
    This reverts commit ebce6b437faeea535908eba19b361e7458845497.

--- hello_world/urls.py
+++ hello_world/urls.py
@@ -1,4 +1,4 @@
-"""brgy URL Configuration
+"""hello_world URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from brgy.core import views as core_views
+from hello_world.core import views as core_views
 
 urlpatterns = [
     path("", core_views.index),
