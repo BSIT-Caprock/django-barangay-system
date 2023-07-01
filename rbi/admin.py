98390b09cc5c83a68408448cd4924b5a923f84a0
jmarkfen <119049235+jmarkfen@users.noreply.github.com>
2023-07-01 17:09:32 +0800

    #3 migrate and register models

--- rbi/admin.py
+++ rbi/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from rbi import models as rbi
+
 # Register your models here.
+
+admin.site.register(rbi.Household)
+admin.site.register(rbi.Individual)
--- rbi/apps.py
+++ rbi/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class RbiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'rbi'
+    verbose_name = 'Records of Barangay Inhabitants'
