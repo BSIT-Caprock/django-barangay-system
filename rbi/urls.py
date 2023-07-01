bf8a8b88db1433d99fa133b73c9ffd040d127873
jmarkfen <119049235+jmarkfen@users.noreply.github.com>
2023-07-01 13:05:41 +0800

    Create urls.py for rbi

--- rbi/urls.py
+++ rbi/urls.py
@@ -1,4 +1,4 @@
-"""brgy URL Configuration
+"""rbi URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -13,14 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
 
-from brgy.core import views as core_views
+from rbi import views
 
 urlpatterns = [
-    path("", core_views.index),
-    path("rbi/", include("rbi.urls")),
-    path("admin/", admin.site.urls),
-    path("__reload__/", include("django_browser_reload.urls")),
+    path("hello", views.hello_world, name="hello"),
 ]
