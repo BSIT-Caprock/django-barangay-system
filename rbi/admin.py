76b93f8e9744a327596998a66cbae51dfdc2e924
jmarkfen <119049235+jmarkfen@users.noreply.github.com>
2023-07-01 19:09:08 +0800

    Update admin.py

--- rbi/admin.py
+++ rbi/admin.py
@@ -2,7 +2,16 @@ from django.contrib import admin
 
 from rbi import models as rbi
 
+# ModelAdmins
+
+class HouseholdAdmin(admin.ModelAdmin):
+    list_display = ("household_number", "date_accomplished", "prepared_by", )
+
+
+class IndividualAdmin(admin.ModelAdmin):
+    list_display = ("last_name", "first_name", "middle_name", "household_number")
+
 # Register your models here.
 
-admin.site.register(rbi.Household)
-admin.site.register(rbi.Individual)
+admin.site.register(rbi.Household, HouseholdAdmin)
+admin.site.register(rbi.Individual, IndividualAdmin)
